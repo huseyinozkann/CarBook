c4bd26e45f5bf8ac7444214eebe168ba1137da48
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-02-02 00:54:25 +0300

    Banner class changed

--- Core/CarBook.Domain/Entities/Banner.cs
+++ Core/CarBook.Domain/Entities/Banner.cs
@@ -3,9 +3,16 @@ namespace CarBook.Domain.Entities
 {
 	public class Banner
 	{
-		public Banner()
-		{
-		}
+		public int BannerID { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string VideoDescription { get; set; }
+
+		public string VideoUrl { get; set; }
+
 	}
 }
 
