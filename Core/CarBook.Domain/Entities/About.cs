e1a02f148a87716d0420df368716ecc54ca4366a
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-02-02 02:19:10 +0300

    About class added

---
+++ Core/CarBook.Domain/Entities/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CarBook.Domain.Entities
+{
+	public class About
+	{
+		public int AboutID { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageUrl { get; set; }
+
+	}
+}
+
