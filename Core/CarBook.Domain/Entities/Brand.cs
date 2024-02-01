099708f0f0e1491f9f8e7ca1eff8291e8ea809c8
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-02-02 01:31:08 +0300

    Car and Brand Class changed

--- Core/CarBook.Domain/Entities/Brand.cs
+++ Core/CarBook.Domain/Entities/Brand.cs
@@ -3,9 +3,12 @@ namespace CarBook.Domain.Entities
 {
 	public class Brand
 	{
-		public Brand()
-		{
-		}
+		public int BrandID { get; set; }
+
+		public string Name { get; set; }
+
+		public List<Car> Cars { get; set; }
+
 	}
 }
 
