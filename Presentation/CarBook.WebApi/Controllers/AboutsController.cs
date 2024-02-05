0397b4f6ca9125e6de0a9e2a8a74e0fbd9ec3da0
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-02-06 02:35:25 +0300

    About API created

---
+++ Presentation/CarBook.WebApi/Controllers/AboutsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBook.Application.Features.CQRS.Queries.AboutQueries;
+using CarBook.Application.Features.CQRS.Commands.AboutCommands;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AboutsController : ControllerBase
+    {
+        private readonly CreateAboutCommandHandler _createAboutCommandHandler;
+        private readonly GetAboutByIdQueryHandler _getAboutByIdQueryHandler;
+        private readonly GetAboutQueryHandler _getAboutQueryHandler;
+        private readonly UpdateAboutCommandHandler _updateAboutCommandHandler;
+        private readonly RemoveAboutCommandHandler _removeAboutCommandHandler;
+
+        public AboutsController(CreateAboutCommandHandler createAboutCommandHandler,
+            GetAboutByIdQueryHandler getAboutByIdQueryHandler, GetAboutQueryHandler getAboutQueryHandler,
+            UpdateAboutCommandHandler updateAboutCommandHandler, RemoveAboutCommandHandler removeAboutCommandHandler)
+        {
+            _createAboutCommandHandler = createAboutCommandHandler;
+            _getAboutByIdQueryHandler = getAboutByIdQueryHandler;
+            _getAboutQueryHandler = getAboutQueryHandler;
+            _updateAboutCommandHandler = updateAboutCommandHandler;
+            _removeAboutCommandHandler = removeAboutCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutList()
+        {
+            var values = await _getAboutQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAbout(int id)
+        {
+            var value = await _getAboutByIdQueryHandler.Handle(new GetAboutByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutCommand command)
+        {
+            await _createAboutCommandHandler.Handle(command);
+            return Ok("Hakkımda Bilgisi Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveAbout(int id)
+        {
+            await _removeAboutCommandHandler.Handle(new RemoveAboutCommand(id));
+            return Ok("Hakkımda Bilgisi Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutCommand command)
+        {
+            await _updateAboutCommandHandler.Handle(command);
+            return Ok("Hakkımda Bilgisi Güncellendi");
+        }
+    }
+}
+
