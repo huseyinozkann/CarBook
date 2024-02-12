2eef56dffbc0b1cc1e3e1a6a94ea664f7c7c6130
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-02-12 15:03:46 +0300

    Location API created

---
+++ Presentation/CarBook.WebApi/Controllers/LocationsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarBook.Application.Features.Mediator.Commands.LocationCommands;
+using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LocationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LocationList()
+        {
+            var values = await _mediator.Send(new GetLocationQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLocation(int id)
+        {
+            var value = await _mediator.Send(new GetLocationByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation(CreateLocationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Lokasyon başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveLocation(int id)
+        {
+            await _mediator.Send(new RemoveLocationCommand(id));
+            return Ok("Lokasyon başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateLocation(UpdateLocationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Lokasyon başarıyla güncellendi");
+        }
+    }
+}
+
