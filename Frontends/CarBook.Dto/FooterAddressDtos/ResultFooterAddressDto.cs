30bd39dde96dbcd1c141ec611b8b04c13896f4b5
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-02-17 15:58:23 +0300

    Footer Address Component

---
+++ Frontends/CarBook.Dto/FooterAddressDtos/ResultFooterAddressDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CarBook.Dto.FooterAddressDtos
+{
+	public class ResultFooterAddressDto
+	{
+        public int FooterAddressID { get; set; }
+
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
+
---
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
