c7ae211b8ea0d0aad02e4fd59d229a02ceee7e9e
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-02-19 16:49:16 +0300

    Service Component and Last 5 Car Component

--- Frontends/CarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/CarBook.WebUI/Controllers/ServiceController.cs
@@ -1,31 +1,15 @@
 ﻿using System;
-using CarBook.Dto.ServiceDtos;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+
 
 namespace CarBook.WebUI.Controllers
 {
 	public class ServiceController : Controller
 	{
-		private readonly IHttpClientFactory _httpClientFactory;
-
-        public ServiceController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-
-        public async Task<IActionResult> Index()
+		public IActionResult Index()
 		{
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7044/api/Services");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
-                return View(values);
-            }
-            return View();
-        }
+			return View();
+		}
 	}
 }
 
