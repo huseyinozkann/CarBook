8c10e962a895cd219d9e7cf1960af9a0bb919055
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-03-01 00:32:18 +0300

    Blog Category, Recent, Tag Cloud Component added

---
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace CarBook.WebUI.ViewComponents.BlogViewComponents
+{
+	public class _BlogDetailsTagCloudComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
+
