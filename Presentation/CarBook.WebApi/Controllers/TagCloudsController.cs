faf194233c1445c45d221cfbaa01accfcb3ff458
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-03-01 23:15:55 +0300

    Tag Cloud API created

---
+++ Presentation/CarBook.WebApi/Controllers/TagCloudsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarBook.Application.Features.Mediator.Commands.TagCloudCommands;
+using CarBook.Application.Features.Mediator.Queries.TagCloudQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagCloudsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TagCloudsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TagCloudList()
+        {
+            var values = await _mediator.Send(new GetTagCloudQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTagCloud(int id)
+        {
+            var value = await _mediator.Send(new GetTagCloudByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTagCloud(CreateTagCloudCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Etiket Bulutu başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveTagCloud(int id)
+        {
+            await _mediator.Send(new RemoveTagCloudCommand(id));
+            return Ok("Etiket Bulutu başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTagCloud(UpdateTagCloudCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Etiket Bulutu başarıyla güncellendi");
+        }
+    }
+}
+
