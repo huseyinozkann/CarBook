8fba870262a3f4f50a572109115f945af04a60a7
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-03-02 00:47:26 +0300

    Get Tag Cloud by Blog Id

---
+++ Core/CarBook.Application/Features/Mediator/Queries/TagCloudQueries/GetTagCloudByBlogIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MediatR;
+using CarBook.Application.Features.Mediator.Results.TagCloudResults;
+namespace CarBook.Application.Features.Mediator.Queries.TagCloudQueries
+{
+	public class GetTagCloudByBlogIdQuery : IRequest<List<GetTagCloudByBlogIdQueryResult>>
+	{
+		public int Id { get; set; }
+
+        public GetTagCloudByBlogIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
+
---
+++ Infrastructure/CarBook.Persistence/Repositories/TagCloudRepositories/TagCloudRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CarBook.Application.Interfaces.TagCloudInterfaces;
+using CarBook.Domain.Entities;
+using CarBook.Persistence.Context;
+namespace CarBook.Persistence.Repositories.TagCloudRepositories
+{
+    public class TagCloudRepository : ITagCloudRepository
+    {
+        private readonly CarBookContext _context;
+
+        public TagCloudRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public List<TagCloud> GetTagCloudsByBlogID(int id)
+        {
+            var values = _context.TagClouds.Where(x => x.BlogID == id).ToList();
+            return values;
+        }
+    }
+}
+
