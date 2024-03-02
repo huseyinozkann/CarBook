c42102f199354617913727bcac0d8168942ee6be
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-03-02 22:54:37 +0300

    Blog Author Dtos and Component

---
+++ Frontends/CarBook.Dto/AuthorDtos/GetAuthorByBlogAuthorIdDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CarBook.Dto.AuthorDtos
+{
+	public class GetAuthorByBlogAuthorIdDto
+	{
+        public int BlogID { get; set; }
+
+        public int AuthorID { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string AuthorDescription { get; set; }
+
+        public string AuthorImageUrl { get; set; }
+    }
+}
+
