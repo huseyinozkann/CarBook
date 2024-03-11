a406c127023d61a0fe0537d90e473a60234c9f59
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-03-11 23:55:38 +0300

    Admin Feature Index added

--- Frontends/CarBook.WebUI/Controllers/AdminFeatureController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminFeatureController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarBook.Dto.FeatureDtos;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +12,24 @@ namespace CarBook.WebUI.Controllers
 {
     public class AdminFeatureController : Controller
     {
-        
-        public IActionResult Index()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminFeatureController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7044/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
