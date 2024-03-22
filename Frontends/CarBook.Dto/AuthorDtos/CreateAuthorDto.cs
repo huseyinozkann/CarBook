2541d5b57b2395255724ad90245a2a3447bda2b9
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-03-23 02:51:15 +0300

    Author Dtos updated

---
+++ Frontends/CarBook.Dto/AuthorDtos/CreateAuthorDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CarBook.Dto.AuthorDtos
+{
+	public class CreateAuthorDto
+	{
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; } 
+    }
+}
+
---
+++ Frontends/CarBook.Dto/AuthorDtos/UpdateAuthorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CarBook.Dto.AuthorDtos
+{
+	public class UpdateAuthorDto
+	{
+        public int AuthorID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+    }
+}
+
