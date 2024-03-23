fa1544b38d66a17813f1c40b60bb9d74187a4bed
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-03-23 13:17:34 +0300

    Admin Author Controller added

--- Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
+++ Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+086ab677ad6d3d8fadb79f7d47ce464869eae613")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
