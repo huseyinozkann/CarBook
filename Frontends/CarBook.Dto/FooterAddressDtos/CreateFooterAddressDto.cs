6e1b1bbc97a2b9334e0f90424991b70f9bb5f6bc
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-09 03:55:20 +0300

    Create and Update Footer Address Dto added

---
+++ Frontends/CarBook.Dto/FooterAddressDtos/CreateFooterAddressDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CarBook.Dto.FooterAddressDtos
+{
+	public class CreateFooterAddressDto
+	{
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
+
--- Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650afe8dfd0255f196a60f1681df897b8a2a34d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
