66266258532ec7fa4a0cbfa8d891bf0741ed524e
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-10 11:32:15 +0300

    Create and Update Testimonials Dtos added

---
+++ Frontends/CarBook.Dto/TestimonialDtos/CreateTestimonialDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CarBook.Dto.TestimonialDtos
+{
+	public class CreateTestimonialDto
+	{
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Comment { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
+
--- Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b558adf0f8872047d778c65f5c322af707d15faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
