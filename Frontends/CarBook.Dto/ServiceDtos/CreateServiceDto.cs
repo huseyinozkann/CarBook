d0a3128d2c7b08fb80d7dd984bdb0ad0092f54ab
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-10 08:12:43 +0300

    Create and Update Service Dto added

---
+++ Frontends/CarBook.Dto/ServiceDtos/CreateServiceDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CarBook.Dto.ServiceDtos
+{
+	public class CreateServiceDto
+	{
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string IconUrl { get; set; }
+    }
+}
+
--- Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
+++ Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1223ba4fe0e7f8eed656a7b0389e0b4ff479f1b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
