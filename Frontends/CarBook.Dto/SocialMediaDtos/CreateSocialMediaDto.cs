197fcd28c9da07fdc526fa6f5b1f1fa34efc320f
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-10 10:18:36 +0300

    Social Media Dtos added

---
+++ Frontends/CarBook.Dto/SocialMediaDtos/CreateSocialMediaDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CarBook.Dto.SocialMediaDtos
+{
+	public class CreateSocialMediaDto
+	{
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
+
---
+++ Frontends/CarBook.Dto/SocialMediaDtos/ResultSocialMediaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CarBook.Dto.SocialMediaDtos
+{
+	public class ResultSocialMediaDto
+	{
+        public int SocialMediaID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
+
