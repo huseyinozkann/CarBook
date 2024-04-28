7eeb3391ce056bdd54b0202e601c71f4ea2c63d8
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-28 18:48:53 +0300

    Admin Statistic created

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminStatistics")]
+    public class AdminStatisticsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminStatisticsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+
+        }
+    }
+}
+
