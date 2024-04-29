5684828ed46e8a6efc889d05885ffae2b41fb442
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-04-29 22:45:15 +0300

    Statistics Repository added

---
+++ Infrastructure/CarBook.Persistence/Repositories/StatisticsRepositories/StatisticsRepository.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using CarBook.Application.Interfaces.StatisticsInterfaces;
+using CarBook.Persistence.Context;
+namespace CarBook.Persistence.Repositories.StatisticsRepositories
+{
+    public class StatisticsRepository : IStatisticsRepository
+    {
+        private readonly CarBookContext _context;
+
+        public StatisticsRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public int GetAuthorCount()
+        {
+            var value = _context.Authors.Count();
+            return value;
+        }
+
+        public decimal GetAvgRentPriceForDaily()
+        {
+            int id = _context.Pricings.Where(y => y.Name == "Günlük").Select(z => z.PricingID).FirstOrDefault();
+            var value = _context.CarPricings.Where(w => w.PricingID == id).Average(x => x.Amount);
+            return value;
+        }
+
+        public decimal GetAvgRentPriceForMonthly()
+        {
+            int id = _context.Pricings.Where(y => y.Name == "Aylık").Select(z => z.PricingID).FirstOrDefault();
+            var value = _context.CarPricings.Where(w => w.PricingID == id).Average(x => x.Amount);
+            return value;
+        }
+
+        public decimal GetAvgRentPriceForWeekly()
+        {
+            int id = _context.Pricings.Where(y => y.Name == "Haftalık").Select(z => z.PricingID).FirstOrDefault();
+            var value = _context.CarPricings.Where(w => w.PricingID == id).Average(x => x.Amount);
+            return value;
+        }
+
+        public int GetBlogCount()
+        {
+            var value = _context.Blogs.Count();
+            return value;
+        }
+
+        public string GetBlogTitleByMaxBlogComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBrandCount()
+        {
+            var value = _context.Brands.Count();
+            return value;
+        }
+
+        public string GetBrandNameByMaxCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCarBrandAndModelByRentPriceDailyMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCarBrandAndModelByRentPriceDailyMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCarCount()
+        {
+            var value = _context.Cars.Count();
+            return value;
+        }
+
+        public int GetCarCountByFuelElectric()
+        {
+            var value = _context.Cars.Where(x => x.Fuel == "Elektrik").Count();
+            return value;
+        }
+
+        public int GetCarCountByFuelGasolineOrDiesel()
+        {
+            var value = _context.Cars.Where(x => x.Fuel == "Benzin" || x.Fuel == "Dizel").Count();
+            return value;
+        }
+
+        public int GetCarCountByKmSmallerThen1000()
+        {
+            var value = _context.Cars.Where(x => x.Km <= 1000).Count();
+            return value;
+        }
+
+        public int GetCarCountByTranmissionIsAuto()
+        {
+            var value = _context.Cars.Where(x => x.Transmission == "Otomatik").Count();
+            return value;
+        }
+
+        public int GetLocationCount()
+        {
+            var value = _context.Locations.Count();
+            return value;
+        }
+    }
+}
+
