6411192453d9bfa5a52b7d9379cdfefe2894572e
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-01 01:30:52 +0300

    Statistics Handlers completed

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/StatisticsHandlers/GetBlogTitleByMaxBlogCommentQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CarBook.Application.Features.Mediator.Queries.StatisticsQueries;
+using CarBook.Application.Features.Mediator.Results.StatisticsResults;
+using CarBook.Application.Interfaces.StatisticsInterfaces;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+	public class GetBlogTitleByMaxBlogCommentQueryHandler : IRequestHandler<GetBlogTitleByMaxBlogCommentQuery, GetBlogTitleByMaxBlogCommentQueryResult>
+	{
+        private readonly IStatisticsRepository _repository;
+
+        public GetBlogTitleByMaxBlogCommentQueryHandler(IStatisticsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetBlogTitleByMaxBlogCommentQueryResult> Handle(GetBlogTitleByMaxBlogCommentQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetBlogTitleByMaxBlogComment();
+            return new GetBlogTitleByMaxBlogCommentQueryResult
+            {
+                BlogTitleByMaxBlogComment = value
+            };
+        }
+    }
+}
+
---
+++ Core/CarBook.Application/Features/Mediator/Handlers/StatisticsHandlers/GetCarCountByTranmissionIsAutoQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CarBook.Application.Features.Mediator.Queries.StatisticsQueries;
+using CarBook.Application.Features.Mediator.Results.StatisticsResults;
+using CarBook.Application.Interfaces.StatisticsInterfaces;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetCarCountByTranmissionIsAutoQueryHandler : IRequestHandler<GetCarCountByTranmissionIsAutoQuery, GetCarCountByTranmissionIsAutoQueryResult>
+    {
+        private readonly IStatisticsRepository _repository;
+
+        public GetCarCountByTranmissionIsAutoQueryHandler(IStatisticsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCarCountByTranmissionIsAutoQueryResult> Handle(GetCarCountByTranmissionIsAutoQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetCarCountByTranmissionIsAuto();
+            return new GetCarCountByTranmissionIsAutoQueryResult
+            {
+                CarCountByTranmissionIsAuto = value
+            };
+        }
+    }
+}
+
