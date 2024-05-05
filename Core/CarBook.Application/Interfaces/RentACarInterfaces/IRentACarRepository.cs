574aacf0349ff87740e7fadedb2094c6312187e1
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-05 13:02:12 +0300

    Rent A Car Interfaces added

---
+++ Core/CarBook.Application/Interfaces/RentACarInterfaces/IRentACarRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CarBook.Domain.Entities;
+using System.Linq.Expressions;
+namespace CarBook.Application.Interfaces.RentACarInterfaces
+{
+	public interface IRentACarRepository
+	{
+		Task<List<RentACar>> GetByFilterAsync(Expression<Func<RentACar, bool>> filter);
+	}
+}
+
