412ee6aac18621f2e5846d36662a48941f1d76fb
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-07 17:32:50 +0300

    Filter Car Rent A Car Dto updated

--- Frontends/CarBook.Dto/RentACarDtos/FilterRentACarDto.cs
+++ Frontends/CarBook.Dto/RentACarDtos/FilterRentACarDto.cs
@@ -3,9 +3,15 @@ namespace CarBook.Dto.RentACarDtos
 {
 	public class FilterRentACarDto
 	{
-		public int locationID { get; set; }
+        public int carID { get; set; }
 
-		public bool available { get; set; }
-	}
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string CoverImageUrl { get; set; }
+    }
 }
 
