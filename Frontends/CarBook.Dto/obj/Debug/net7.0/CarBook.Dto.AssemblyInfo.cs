b9af619e303f99140601a55d62bc79c4dc7b7ede
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-08 18:13:54 +0300

    Navbar UI Links updated

--- Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908eae17e27e82f7fbd6f9a22abca123cd1bb73b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fd96698fe7f996cbfea53b443631e1ac377932")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
