79c03ed03ae0f36f06962e2ed066af3c4032cbc6
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-09 15:44:08 +0300

    Reservation Controller added

---
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ReservationController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
