34356e2b3dd43dbcfc93140384526edd7547e6d9
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-11 11:55:17 +0300

    Reservation Controller added

---
+++ Presentation/CarBook.WebApi/Controllers/ReservationsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using CarBook.Application.Features.Mediator.Commands.ReservationCommands;
+
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReservationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReservation(CreateReservationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Rezervasyon başarıyla eklendi");
+        }
+    }
+}
+
