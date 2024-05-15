3bd98a71dcff07296fb10ebad6f272013c92cd29
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-15 18:46:54 +0300

    Car Pricing with Time Period Handler updated

--- Core/CarBook.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithTimePeriodQueryHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithTimePeriodQueryHandler.cs
@@ -20,7 +20,16 @@ namespace CarBook.Application.Features.Mediator.Handlers.CarPricingHandlers
 
         public async Task<List<GetCarPricingWithTimePeriodQueryResult>> Handle(GetCarPricingWithTimePeriodQuery request, CancellationToken cancellationToken)
         {
-            
+            var values = _repository.GetCarPricingWithTimePeriod1();
+            return values.Select(x => new GetCarPricingWithTimePeriodQueryResult
+            {
+                Brand = x.Brand,
+                Model = x.Model,
+                CoverImageUrl = x.CoverImageUrl,
+                DailyAmount = x.Amounts[0],
+                WeeklyAmount = x.Amounts[1],
+                MonthlyAmount = x.Amounts[2]
+            }).ToList();
         }
     }
 }
