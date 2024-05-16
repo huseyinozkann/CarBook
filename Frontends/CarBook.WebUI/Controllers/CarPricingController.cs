e4220e550ac17583574623e9fc22f17893822920
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-16 12:36:42 +0300

    Car Pricing Controller Index updated

--- Frontends/CarBook.WebUI/Controllers/CarPricingController.cs
+++ Frontends/CarBook.WebUI/Controllers/CarPricingController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarBook.Dto.CarPricingDtos;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 
 
@@ -10,11 +12,23 @@ namespace CarBook.WebUI.Controllers
 {
     public class CarPricingController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CarPricingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Paketler";
             ViewBag.v2 = "Araç Fiyat Paketleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7060/api/CarPricings/GetCarPricingWithTimePeriodList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
