bdff4b1048c7cc2790c97004b2729409a5f103fd
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-17 18:17:04 +0300

    İstatistics View updated

--- Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7477f6436506610e55a72852b22569e3cc6c073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
