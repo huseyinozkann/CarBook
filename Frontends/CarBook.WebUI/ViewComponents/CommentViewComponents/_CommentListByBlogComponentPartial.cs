c461f09be83cdcf6ee79b94ce89bc01ccf778d53
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-19 22:01:09 +0300

    CommentsListByBlogComponentPartial View updated

--- Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using CarBook.Dto.CommentDtos;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 namespace CarBook.WebUI.ViewComponents.CommentViewComponents
 {
 	public class _CommentListByBlogComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.blogid = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7060/api/Comments/CommentListByBlog?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
 }
 
--- Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
+++ Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea5b7452f537e984fdf2cb742286a149a1bc1bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
