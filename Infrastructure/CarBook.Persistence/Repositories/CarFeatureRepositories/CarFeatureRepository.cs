b3190ecfa7d7c9be9df2e6260ec788c430f0117e
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-21 23:59:46 +0300

    Car Feature Repository added

---
+++ Infrastructure/CarBook.Persistence/Repositories/CarFeatureRepositories/CarFeatureRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CarBook.Application.Interfaces.CarFeatureInterfaces;
+using CarBook.Domain.Entities;
+using CarBook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBook.Persistence.Repositories.CarFeatureRepositories
+{
+    public class CarFeatureRepository : ICarFeatureRepository
+    {
+        private readonly CarBookContext _context;
+
+        public CarFeatureRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public List<CarFeature> GetCarFeaturesByCarID(int carID)
+        {
+            var values = _context.CarFeatures.Include(y => y.Feature).Where(x => x.CarID == carID).ToList();
+            return values;
+        }
+    }
+}
+
