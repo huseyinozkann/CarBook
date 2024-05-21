d795a8c4a76051e0679d4f689d4de2ee489d8c45
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-22 02:14:38 +0300

    Admin Car Feature Detail Controller Index View Updated

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminCarFeatureDetailController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminCarFeatureDetailController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using CarBook.Dto.CarFeatureDtos;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
 
 
 namespace CarBook.WebUI.Areas.Admin.Controllers
@@ -12,9 +14,24 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
     [Route("Admin/AdminCarFeatureDetail")]
     public class AdminCarFeatureDetailController : Controller
     {
-        [Route("Index/{id}")]
-        public IActionResult Index(int id)
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminCarFeatureDetailController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> Index(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7044/api/CarFeatures?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
