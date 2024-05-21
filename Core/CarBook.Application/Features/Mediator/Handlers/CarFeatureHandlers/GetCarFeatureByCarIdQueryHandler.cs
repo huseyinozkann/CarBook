1d8caef3dc24a976e0c656f98c4c2e186a072c87
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-22 00:37:57 +0300

    Car Feature Mediator added

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/CarFeatureHandlers/GetCarFeatureByCarIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CarBook.Application.Features.Mediator.Queries.CarFeatureQueries;
+using CarBook.Application.Features.Mediator.Results.CarFeatureResults;
+using CarBook.Application.Interfaces.CarFeatureInterfaces;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.CarFeatureHandlers
+{
+    public class GetCarFeatureByCarIdQueryHandler : IRequestHandler<GetCarFeatureByCarIdQuery, List<GetCarFeatureByCarIdQueryResult>>
+    {
+        private readonly ICarFeatureRepository _repository;
+        public GetCarFeatureByCarIdQueryHandler(ICarFeatureRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetCarFeatureByCarIdQueryResult>> Handle(GetCarFeatureByCarIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetCarFeaturesByCarID(request.Id);
+            return values.Select(x => new GetCarFeatureByCarIdQueryResult
+            {
+                Available = x.Available,
+                CarFeatureID = x.CarFeatureID,
+                FeatureID = x.FeatureID,
+                FeatureName = x.Feature.Name
+            }).ToList();
+        }
+    }
+}
+
