b36cc54d18af83ed15ea814de622ae8a2d2a0ade
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-25 13:33:07 +0300

    ICar Feature Repository Interfaces completed

--- Core/CarBook.Application/Interfaces/CarFeatureInterfaces/ICarFeatureRepository.cs
+++ Core/CarBook.Application/Interfaces/CarFeatureInterfaces/ICarFeatureRepository.cs
@@ -6,6 +6,9 @@ namespace CarBook.Application.Interfaces.CarFeatureInterfaces
 	public interface ICarFeatureRepository
 	{
         List<CarFeature> GetCarFeaturesByCarID(int carID);
+        void ChangeCarFeatureAvailableToFalse(int id);
+        void ChangeCarFeatureAvailableToTrue(int id);
+        void CreateCarFeatureByCar(CarFeature carFeature);
     }
 }
 
