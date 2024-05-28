3eb45ca76b892d5d67f59a1b5c67516d5ab7ff42
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-29 01:19:40 +0300

    Car Detail Tab Pane Component Partial added

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailTabPaneComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailTabPaneComponentPartial : ViewComponent
+    {
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
+
