f519cfe04760a64bcfbe6d49a1b654d2fb011f2d
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-28 23:07:07 +0300

    Car Detail Controller added

--- Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
+++ Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d4f9a7b66a330aeededf8c863fa969107db59e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
