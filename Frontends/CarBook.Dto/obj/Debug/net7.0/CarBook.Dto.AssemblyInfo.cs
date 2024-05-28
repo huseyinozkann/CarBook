0ddf1e8161f990c83e2ad25200a78e4b7c0c3c63
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-28 17:40:42 +0300

    Admin Statistics Component View added

--- Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75ba86420f95cbadd09bb8260aa5d364b6acc57")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
