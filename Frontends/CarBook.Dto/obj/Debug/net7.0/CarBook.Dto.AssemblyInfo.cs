ccabcd711537bbfade3214ecf1e3b16bdeeef3e7
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-29 13:32:44 +0300

    Car Detail Car Feature by CarId Component Partial added

--- Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net7.0/CarBook.Dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d4f9a7b66a330aeededf8c863fa969107db59e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
