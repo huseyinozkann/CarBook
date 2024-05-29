8f61d209f5e62d795d8d4f8caaa1359b1abcf7df
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-29 16:15:14 +0300

    Car Description Mediator added

---
+++ Core/CarBook.Application/Features/Mediator/Results/CarDescriptionResults/GetCarDescriptionQueryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CarBook.Application.Features.Mediator.Results.CarDescriptionResults
+{
+	public class GetCarDescriptionQueryResult
+	{
+        public int CarDescriptionID { get; set; }
+
+        public int CarID { get; set; }
+
+        public string Details { get; set; }
+    }
+}
+
