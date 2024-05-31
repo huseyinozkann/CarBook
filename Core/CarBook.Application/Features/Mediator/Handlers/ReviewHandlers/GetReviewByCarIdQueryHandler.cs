b99569a3669066bedd51ac5478d2ccfe2628f367
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-31 15:27:03 +0300

    Review Mediator added

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/ReviewHandlers/GetReviewByCarIdQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CarBook.Application.Features.Mediator.Queries.ReviewQueries;
+using CarBook.Application.Features.Mediator.Results.ReviewResults;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.ReviewHandlers
+{
+    public class GetReviewByCarIdQueryHandler : IRequestHandler<GetReviewByCarIdQuery, List<GetReviewByCarIdQueryResult>>
+    {
+        public Task<List<GetReviewByCarIdQueryResult>> Handle(GetReviewByCarIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
