fc526b3db4c2308e419de065c5f49a12693bdacf
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-05-31 16:32:34 +0300

    Reviews Controller added

---
+++ Presentation/CarBook.WebApi/Controllers/ReviewsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using CarBook.Application.Features.Mediator.Queries.ReviewQueries;
+
+
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReviewsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ReviewListByCarId(int id)
+        {
+            var values = await _mediator.Send(new GetReviewByCarIdQuery(id));
+            return Ok(values);
+        }
+    }
+}
+
