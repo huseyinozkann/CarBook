b241da79a2c7a8978c0cc83662d7e26135f58918
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-06-01 18:10:43 +0300

    Create and Update Review Handler added

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/ReviewHandlers/CreateReviewHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CarBook.Application.Features.Mediator.Commands.ReviewCommands;
+using MediatR;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.Mediator.Handlers.ReviewHandlers
+{
+    public class CreateReviewHandler : IRequestHandler<CreateReviewCommand>
+    {
+        private readonly IRepository<Review> _repository;
+
+        public CreateReviewHandler(IRepository<Review> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateReviewCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Review
+            {
+                CustomerImage = request.CustomerImage,
+                CarID = request.CarID,
+                Comment = request.Comment,
+                CustomerName = request.CustomerName,
+                RaytingValue = request.RaytingValue,
+                ReviewDate = DateTime.Parse(DateTime.Now.ToShortDateString())
+            });
+        }
+    }
+}
+
