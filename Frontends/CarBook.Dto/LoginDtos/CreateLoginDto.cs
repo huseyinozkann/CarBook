79f6baaaace066026ae6f7b9e98113aae7c7b403
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-04 16:46:24 +0300

    Login Dtos added

---
+++ Frontends/CarBook.Dto/LoginDtos/CreateLoginDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CarBook.Dto.LoginDtos
+{
+	public class CreateLoginDto
+	{
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
