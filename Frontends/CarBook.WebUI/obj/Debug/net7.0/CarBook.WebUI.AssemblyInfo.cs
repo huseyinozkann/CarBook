9e8b2045b88340d0dbdd2068c7016fc82793c621
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-04 09:01:07 +0300

    Create App User View added

--- Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
+++ Frontends/CarBook.WebUI/obj/Debug/net7.0/CarBook.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9921b10e64143fa099fadef12d28385c01b20a8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
